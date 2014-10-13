740d4807e9b5e9a1585fa35011ed9c1eeb16dd19
miriam capitan roca <mcapitan@iota41.aulas.gsyc.es>
2014-10-13 10:14:10 +0200

    Añado ejercicio 5

--- karaoke.py
+++ karaoke.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
 
-from smallsmilhandler import SmallSMILHandler
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import smallsmilhandler
 import sys
 import os
 
 parser = make_parser()
-sHandler = SmallSMILHandler()
-parser.setContentHandler(sHandler)
-parser.parse(open(sys.argv[1]))
-lista = sHandler.get_tags()
+lista_SSH = smallsmilhandler.SmallSMILHandler()
+parser.setContentHandler(lista_SSH)
+parser.parse(open('karaoke.smil'))    
 
-if len(sys.argv) != 2:
-    sys.exit("Usage: python karaoke.py file.smil")
+lista = lista_SSH.get_tags()
 
-val_atr = []
+val_atri = []
 for i in range(len(lista)):
     if type(lista[i]) == dict:
         diccionario = lista[i]
         lista_tupla = diccionario.items()
+
         for j in range(len(lista_tupla)):
-            val_atr.append(str((lista_tupla[j])[0]) + "=" + 
-            str((lista_tupla[j])[1]))
+            val_atri.append(str((lista_tupla[j])[0]) + "=" + 
+                            str((lista_tupla[j])[1]))
+            if str((lista_tupla[j])[0]) == 'src':
+                recurso = str((lista_tupla[j])[1])
+                if recurso[:7] == "http://":
+		            os.system("wget -q " + recurso)
             if j == (len(lista_tupla) - 1):
-                print "\t".join(val_atr)
-                val_atr = []
+                print "\t".join(val_atri)
+                val_atri = []
     else:
-        val_atr.append(lista[i])
+        val_atri.append(lista[i])
